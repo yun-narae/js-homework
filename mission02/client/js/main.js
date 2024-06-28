1a86811b8957f325851c3122329d8da0281c3124
seulbinim <skfo0827@naver.com>
2024-06-28 23:29:45 +0900

    06.28 과제중

---
+++ mission02/client/js/main.js
@@ -0,0 +1,60 @@
+import { data } from './data.js';
+
+/* 
+
+1. 클릭 이벤트 활성화
+2. nav 클릭시 배경 색상 변경
+3. 이미지 변경
+4. 텍스트 변경
+5. 함수 분리
+
+*/
+
+
+
+const nav = $('.nav');
+
+nav.on('click', 'li', function(e){
+    e.preventDefault();
+    
+    // console.log(this);
+
+    const index = parseInt($(this).attr('data-index'),10);
+    // console.log(index);
+    const item = data[index - 1];
+    console.log(item);
+
+    $('.nav li').removeClass('is-active'); 
+    // active를 제거 먼저 작성한다. 
+    // '.nav li' 가 아니고 'nav > li'로하면 이상하게 작동됨
+
+    $(this).addClass('is-active'); // active Class 추가
+    // console.log(e)
+
+    if (item) {
+        // GPT의 도움을 받고있어요..
+        const nameLowerCase = item.name.toLowerCase();
+
+        $('.visual img').attr({
+          'name': `./assets/${nameLowerCase}.jpeg`,
+          'color': '',
+          'alt': '',
+        });
+      }
+    
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
